aa2df13a3a659a41d56b043331acd2ca8fb73b8c
Jacob Starry <jstarry@mail.bradley.edu>
2021-05-13 08:13:16 -0500

    Initial Commit

---
+++ SwiftUIImageCarousel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIImageCarousel
+//
+//  Created by Jacob Starry on 5/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIImageCarousel/SwiftUIImageCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageCarouselApp.swift
+//  SwiftUIImageCarousel
+//
+//  Created by Jacob Starry on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
