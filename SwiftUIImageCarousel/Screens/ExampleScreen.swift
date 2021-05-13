a59022a9f0ee7b29df16f69d6f8a7ee48bc139c3
Jacob Starry <jstarry@mail.bradley.edu>
2021-05-13 08:41:27 -0500

    added kittens

--- SwiftUIImageCarousel/Screens/ExampleScreen.swift
+++ SwiftUIImageCarousel/Screens/ExampleScreen.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ExampleScreen: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ExampleScreen_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
--- SwiftUIImageCarousel/SwiftUIImageCarouselApp.swift
+++ SwiftUIImageCarousel/SwiftUIImageCarouselApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIImageCarouselApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ExampleScreen()
         }
     }
 }
